60e362d02c64f435b81744eb643cd8a6a82cecd0
joeztang <joeztang7@gmail.com>
2016-05-28 14:07:29 -0700

    test2

--- start-build-env.sh
+++ start-build-env.sh
@@ -14,8 +14,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# sss
+#aaa
 
 set -e               # exit on error
+
 
 cd "$(dirname "$0")" # connect to root
 USER="joe"
