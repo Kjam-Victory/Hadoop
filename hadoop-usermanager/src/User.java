22521c87150911f90647efe30681613ebcd62348
System Administrator <root@JiangKaideMacBook-Pro.local>
2016-06-08 11:27:38 -0700

    Final Stable Debug Version

---
+++ hadoop-usermanager/src/User.java
@@ -0,0 +1,34 @@
+import java.util.*; 
+
+public class User {
+	private String name;
+	private long ip;
+	public User(String name, long ip) {
+		this.name = name;
+		this.ip = ip;
+	}
+	public User(String name, String s) {
+		System.out.println(s);
+		long ip_num = (Long.parseLong(s.split("\\.")[0])<<24) + (Long.parseLong(s.split("\\.")[1])<<16)
+				+ (Long.parseLong(s.split("\\.")[2])<<8) +Long.parseLong(s.split("\\.")[3]); 
+		this.name = name;
+		this.ip = ip_num;
+	}
+	public String getName() {
+		return name;
+	}
+	public long getIP() {
+		return ip;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setIP(long ip) {
+		this.ip = ip;
+	}
+	public void setIP(String s){
+		long ip_num = (Long.parseLong(s.split("\\.")[0])<<24) + (Long.parseLong(s.split("\\.")[1])<<16)
+				+ (Long.parseLong(s.split("\\.")[2])<<8) +Long.parseLong(s.split("\\.")[3]); 
+		this.ip = ip_num;
+	}
+}
